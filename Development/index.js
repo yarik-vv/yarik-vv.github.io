91c02989e2be3d4c71ce618df3a8ca066613f908
yarik-vv <yarik.vashchuk@gmail.com>
2017-06-11 19:23:02 +0300

    update styles new error page

--- Development/index.js
+++ Development/index.js
@@ -2,5 +2,3 @@
 
 import css from './scss/main.scss';
 import favicon from './img/favicon.png';
-import img from './img/error.png';
-var error = require('file-loader?name=[path][name].[ext]!./404.html');
